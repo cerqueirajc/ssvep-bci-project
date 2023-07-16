ef6cd0e0db313b67cb72b86352a80f8f393f5004
Joao Cerqueira <jc.cerqueira13@gmail.com>
2023-07-16 17:02:34 -0300

    Refactor learners, generalize better the models

--- src/ssvepcca/learners.py
+++ src/ssvepcca/learners.py
@@ -6,7 +6,9 @@ from scipy import signal
 from ssvepcca.definitions import TARGET_FREQUENCY, NUM_TARGETS, NUM_SAMPLES, SAMPLE_FREQ
 from ssvepcca.utils import get_harmonic_columns, electrodes_name_to_index
 
+
 CCA_MAX_ITER = 1000
+
 
 class CCACorrelation(CCA):
 
@@ -25,7 +27,7 @@ class CCACorrelation(CCA):
         return self.fit(X, Y).correlation(X, Y, n_components)
 
 
-class CCABase():
+class CCABase:
 
     def __init__(
         self,
@@ -41,6 +43,12 @@ class CCABase():
         self.electrodes_index = electrodes_name_to_index(electrodes_name) if electrodes_name else None
         self.num_components = num_components
         self.num_harmonics = num_harmonics
+        self.harmonic_column = lambda freq: get_harmonic_columns(
+            freq,
+            self.start_time_index,
+            self.stop_time_index,
+            self.num_harmonics
+        )
 
 
     @staticmethod
@@ -52,36 +60,34 @@ class CCABase():
     @staticmethod
     def _check_fit_input(input_array):
         assert input_array.ndim == 4
-        assert input_array.shape[-2] == 1500
+        assert input_array.shape[-2] == NUM_SAMPLES
         assert input_array.shape[1] == NUM_TARGETS
 
 
     @staticmethod
-    def _filter_eeg(eeg, start_time_index, stop_time_index, electrodes_index):
-        eeg_filtered = eeg[..., start_time_index:stop_time_index, :]
+    def _filter_eeg_electrodes(eeg, electrodes_index):
         if electrodes_index is not None:
-            eeg_filtered = eeg_filtered[..., electrodes_index]
-        return eeg_filtered
+            return eeg[..., electrodes_index]
+        return eeg
+
+
+    @staticmethod
+    def _filter_eeg_time(eeg, start_time_index, stop_time_index):
+        return eeg[..., start_time_index:stop_time_index, :]
 
 
     def predict_proba(self, eeg):
         self._check_predict_input(eeg)
 
-        eeg = self._filter_eeg(eeg, self.start_time_index, self.stop_time_index, self.electrodes_index)
+        eeg = self._filter_eeg_electrodes(eeg, self.electrodes_index)
+        eeg = self._filter_eeg_time(eeg, self.start_time_index, self.stop_time_index)
 
-        correlations = np.empty(NUM_TARGETS)
+        correlations = np.empty([NUM_TARGETS, self.num_components])
 
         for target, freq in enumerate(TARGET_FREQUENCY):
-            harmonic = get_harmonic_columns(
-                freq,
-                start_time_index=self.start_time_index,
-                stop_time_index=self.stop_time_index,
-                num_harmonics=self.num_harmonics
-            )
-            # increased max_iter, convergence problems
+            harmonic = self.harmonic_column(freq)
             cca_model = CCACorrelation(n_components=self.num_components, max_iter=CCA_MAX_ITER, scale=False)
-
-            correlations[target] = cca_model.fit_correlation(eeg, harmonic)[0]
+            correlations[target, :] = cca_model.fit_correlation(eeg, harmonic)
 
         return correlations
 
@@ -103,9 +109,17 @@ class CCASingleComponent(CCABase):
             num_harmonics=num_harmonics,
         )
 
+
+    def predict(self, eeg):
+        probability = self.predict_proba(eeg)[:, 0]
+        return probability.argmax(), probability
+
+
+class CCAMultiComponent(CCABase):
+
     def predict(self, eeg):
         probability = self.predict_proba(eeg)
-        return probability.argmax(), probability
+        return probability.mean(axis=1).argmax(), probability
 
 
 class CCAFixedCoefficients(CCASingleComponent):
@@ -113,8 +127,9 @@ class CCAFixedCoefficients(CCASingleComponent):
     def fit(self, eeg_tensor):
 
         self._check_fit_input(eeg_tensor)
-        eeg_tensor = self._filter_eeg(eeg_tensor, self.start_time_index, self.stop_time_index, self.electrodes_index)
-        
+        eeg_tensor = self._filter_eeg_electrodes(eeg_tensor, self.electrodes_index)
+        eeg_tensor = self._filter_eeg_time(eeg_tensor, self.start_time_index, self.stop_time_index)
+
         num_blocks = eeg_tensor.shape[0]
         num_electrodes = eeg_tensor.shape[-1]
 
@@ -124,12 +139,7 @@ class CCAFixedCoefficients(CCASingleComponent):
 
             eeg_concatenated = eeg_tensor[:, target, :, :].reshape(-1, num_electrodes)
 
-            harmonic = get_harmonic_columns(
-                freq,
-                start_time_index=self.start_time_index,
-                stop_time_index=self.stop_time_index,
-                num_harmonics=self.num_harmonics
-            )
+            harmonic = self.harmonic_column(freq)
 
             harmonic_concatenated = (
                 harmonic[np.newaxis, :, :] # create new dummy dimension that will be expanded
@@ -148,19 +158,14 @@ class CCAFixedCoefficients(CCASingleComponent):
     def predict_proba(self, eeg):
 
         self._check_predict_input(eeg)
-        eeg = self._filter_eeg(eeg, self.start_time_index, self.stop_time_index, self.electrodes_index)
+        eeg = self._filter_eeg_electrodes(eeg, self.electrodes_index)
+        eeg = self._filter_eeg_time(eeg, self.start_time_index, self.stop_time_index)
 
-        correlations = np.empty(NUM_TARGETS) # initialize
+        correlations = np.empty([NUM_TARGETS, self.num_components])
 
         for target, freq in enumerate(TARGET_FREQUENCY):
-            harmonic = get_harmonic_columns(
-                freq,
-                start_time_index=self.start_time_index,
-                stop_time_index=self.stop_time_index,
-                num_harmonics=self.num_harmonics
-            )
-
-            correlations[target] = self.cca_models[target].correlation(eeg, harmonic)[0]
+            harmonic = self.harmonic_column(freq)
+            correlations[target, :] = self.cca_models[target].correlation(eeg, harmonic)
 
         return correlations
 
@@ -172,7 +177,7 @@ class FBCCA(CCABase):
     CHEBY_FS = SAMPLE_FREQ
     CHEBY_BAND_TYPE = "bandpass"
     FB_BAND_FREQ_TOL = 2
-    FB_PADDING_TYPE = "odd" #"constant"#
+    FB_PADDING_TYPE = "odd"
 
     def __init__(
         self,
@@ -197,7 +202,7 @@ class FBCCA(CCABase):
 
         self.fb_filters = []
         for i in range(self.fb_num_subband):
-            
+
             freq_low = fb_fundamental_freq * (i + 1)
             freq_high = self.fb_upper_bound_freq
             assert freq_low < freq_high, f"Number of FB subband is too high, try decreasing it"
@@ -209,9 +214,9 @@ class FBCCA(CCABase):
                 btype=self.CHEBY_BAND_TYPE,
                 fs=self.CHEBY_FS
             )
-            
+
             self.fb_filters.append({"b": b, "a": a})
-        
+
         def fb_weight(n):
             a = self.fb_weight__a
             b = self.fb_weight__b
@@ -221,44 +226,37 @@ class FBCCA(CCABase):
 
 
     def predict_proba(self, eeg):
-        
+
         self._check_predict_input(eeg)
 
         # filter electrodes early to avoid filtering unecessary operations
-        if self.electrodes_index:
-            eeg = eeg[:, self.electrodes_index]
-        # filter end date before filtering to avoid unfair data leakage
-        eeg = eeg[:self.stop_time_index, ...]
+        eeg = self._filter_eeg_electrodes(eeg, self.electrodes_index)
+        # filter end date before filtering to avoid data leakage
+        eeg = self._filter_eeg_time(eeg, None, self.stop_time_index)
         # create new axis to be filtered
-        eeg_fb = eeg[:, :, np.newaxis].repeat(self.fb_num_subband, axis=2)
-        # return eeg_fb
+        eeg_fb = (
+            eeg[np.newaxis, :, :]
+            .repeat(self.fb_num_subband, axis=0)
+        )
         # replace eeg with filtered versions
         for filter_idx, filter_params in enumerate(self.fb_filters):
-           for electrode_idx in range(eeg_fb.shape[1]):
-                eeg_fb[:, electrode_idx, filter_idx] = signal.filtfilt(
-                        x=eeg_fb[:, electrode_idx, filter_idx],
-                        b=filter_params["b"],
-                        a=filter_params["a"],
-                        padtype=self.FB_PADDING_TYPE
-                    )
-        # Filter start_time_index after FB, to improve performance due to initial borders
-        eeg_fb = eeg_fb[self.start_time_index:, ...]
+           for electrode_idx in range(len(self.electrodes_index)):
+                eeg_fb[filter_idx, :, electrode_idx] = signal.filtfilt(
+                    x=eeg_fb[filter_idx, :, electrode_idx],
+                    b=filter_params["b"],
+                    a=filter_params["a"],
+                    padtype=self.FB_PADDING_TYPE
+                )
+
+        eeg_fb = self._filter_eeg_time(eeg_fb, self.start_time_index, None)
 
         correlations = np.empty([NUM_TARGETS, self.fb_num_subband])
 
         for target, freq in enumerate(TARGET_FREQUENCY):
-
-            harmonic = get_harmonic_columns(
-                freq,
-                start_time_index=self.start_time_index,
-                stop_time_index=self.stop_time_index,
-                num_harmonics=self.num_harmonics
-            )
-            
+            harmonic = self.harmonic_column(freq)
             for filter_idx in range(self.fb_num_subband):
-
                 cca_model = CCACorrelation(n_components=self.num_components, max_iter=CCA_MAX_ITER, scale=False)
-                correlations[target, filter_idx] = cca_model.fit_correlation(eeg_fb[..., filter_idx], harmonic)[0]
+                correlations[target, filter_idx] = cca_model.fit_correlation(eeg_fb[filter_idx, ...], harmonic)[0]
 
         return correlations
 
