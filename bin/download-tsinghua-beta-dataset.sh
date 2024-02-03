0fcffc32d741b6985638738f3c319a5a0b28434b
Joao Cerqueira <jc.cerqueira13@gmail.com>
2024-02-03 00:31:46 -0300

    Add script to download dataset

--- bin/download-tsinghua-beta-dataset.sh
+++ bin/download-tsinghua-beta-dataset.sh
@@ -1,13 +1,19 @@
 #! /bin/sh
 
-for i in $(seq 1 70)
-do
- curl "http://bci.med.tsinghua.edu.cn/upload/yijun/S$i.mat.7z" --output "~/downloads/tsinghua/S$i.mat.7z" --create-dirs &
-done
+curl "http://bci.med.tsinghua.edu.cn/upload/liubingchuan/S1-S10.tar.gz" --output "/mnt/mystorage/tsinghua_beta_dataset/S1-S10.tar.gz" --create-dirs &
+curl "http://bci.med.tsinghua.edu.cn/upload/liubingchuan/S11-S20.tar.gz" --output "/mnt/mystorage/tsinghua_beta_dataset/S11-S20.tar.gz" --create-dirs &
+curl "http://bci.med.tsinghua.edu.cn/upload/liubingchuan/S21-S30.tar.gz" --output "/mnt/mystorage/tsinghua_beta_dataset/S21-S30.tar.gz" --create-dirs &
+curl "http://bci.med.tsinghua.edu.cn/upload/liubingchuan/S31-S40.tar.gz" --output "/mnt/mystorage/tsinghua_beta_dataset/S31-S40.tar.gz" --create-dirs &
+curl "http://bci.med.tsinghua.edu.cn/upload/liubingchuan/S41-S50.tar.gz" --output "/mnt/mystorage/tsinghua_beta_dataset/S41-S50.tar.gz" --create-dirs &
+curl "http://bci.med.tsinghua.edu.cn/upload/liubingchuan/S51-S60.tar.gz" --output "/mnt/mystorage/tsinghua_beta_dataset/S51-S60.tar.gz" --create-dirs &
+curl "http://bci.med.tsinghua.edu.cn/upload/liubingchuan/S61-S70.tar.gz" --output "/mnt/mystorage/tsinghua_beta_dataset/S61-S70.tar.gz" --create-dirs &
 
 wait
 
-for i in $(seq 1 70)
-do
- 7z x "S$i.mat.7z"
-done
+tar -xvf "/mnt/mystorage/tsinghua_beta_dataset/S1-S10.tar.gz"  -C "/mnt/mystorage/tsinghua_beta_dataset"
+tar -xvf "/mnt/mystorage/tsinghua_beta_dataset/S11-S20.tar.gz" -C "/mnt/mystorage/tsinghua_beta_dataset"
+tar -xvf "/mnt/mystorage/tsinghua_beta_dataset/S21-S30.tar.gz" -C "/mnt/mystorage/tsinghua_beta_dataset"
+tar -xvf "/mnt/mystorage/tsinghua_beta_dataset/S31-S40.tar.gz" -C "/mnt/mystorage/tsinghua_beta_dataset"
+tar -xvf "/mnt/mystorage/tsinghua_beta_dataset/S41-S50.tar.gz" -C "/mnt/mystorage/tsinghua_beta_dataset"
+tar -xvf "/mnt/mystorage/tsinghua_beta_dataset/S51-S60.tar.gz" -C "/mnt/mystorage/tsinghua_beta_dataset"
+tar -xvf "/mnt/mystorage/tsinghua_beta_dataset/S61-S70.tar.gz" -C "/mnt/mystorage/tsinghua_beta_dataset"
