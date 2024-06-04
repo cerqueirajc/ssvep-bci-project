025bfa9962343454060551d585be35fb75c9dea0
Joao Cerqueira <jc.cerqueira13@gmail.com>
2024-06-04 09:35:20 -0300

    Update bin files

--- bin/initialize-machine.sh
+++ bin/initialize-machine.sh
@@ -2,8 +2,10 @@
 sudo apt update
 
 # install miniconda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda-installer.sh
-bash ~/miniconda-installer.sh
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
 
 # create and activate new conda env
 source ~/.bashrc
--- bin/mount-disk.sh
+++ bin/mount-disk.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+# Use the following command to list and find the correct device
+# ls -l /dev/disk/by-id/google-*
 sudo mkdir /mnt/mystorage
 sudo mount -o discard,defaults /dev/nvme0n2 /mnt/mystorage
 sudo chmod a+w /mnt/mystorage
