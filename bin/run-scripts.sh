3df5e8c152044572b57bdb5f24aff8c360ab6a8c
Joao Cerqueira <jc.cerqueira13@gmail.com>
2024-06-05 23:16:18 -0300

    Update bash script to run

--- bin/run-scripts.sh
+++ bin/run-scripts.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-nohup python scripts_v3/exp_benchmark_dataset_delayed.py -u > exp_benchmark_dataset_delayed.log &
-nohup python scripts_v3/exp_beta_dataset_delayed_01_15.py -u > exp_beta_dataset_delayed_01_15.log &
-nohup python scripts_v3/exp_beta_dataset_delayed_16_70.py -u > exp_beta_dataset_delayed_16_70.log &
+nohup python scripts_v3/exp_benchmark_dataset_delayed.py -u > /mnt/mystorage/exp_benchmark_dataset_delayed.log &
+nohup python scripts_v3/exp_beta_dataset_delayed_01_15.py -u > /mnt/mystorage/exp_beta_dataset_delayed_01_15.log &
+nohup python scripts_v3/exp_beta_dataset_delayed_16_70.py -u > /mnt/mystorage/exp_beta_dataset_delayed_16_70.log &
