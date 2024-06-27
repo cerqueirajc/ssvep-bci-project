b33bfb0dbe99df0cad9e0bb7bb85ac098ab6368b
Joao Cerqueira <jc.cerqueira13@gmail.com>
2024-06-26 22:13:05 -0300

    Update scripts

--- bin/initialize-machine.sh
+++ bin/initialize-machine.sh
@@ -6,8 +6,9 @@ mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
-bash ~/miniconda3/bin/conda init bash
-bash ~/miniconda3/bin/conda init zsh
+source ~/miniconda3/bin/activate
+conda init bash
+conda init zsh
 
 # create and activate new conda env
 source ~/.bashrc
--- bin/mount-disk.sh
+++ bin/mount-disk.sh
@@ -2,6 +2,8 @@
 
 # Use the following command to list and find the correct device
 # ls -l /dev/disk/by-id/google-*
+
+sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
 sudo mkdir /mnt/mystorage
-sudo mount -o discard,defaults /dev/nvme0n2 /mnt/mystorage
+sudo mount -o discard,defaults /dev/sdb /mnt/mystorage
 sudo chmod a+w /mnt/mystorage
